5a3f774e070b071b60b08d006ba04384d85b3e18
ravi-PC\ravi <reddy_s35@yahoo.in>
2017-05-22 00:51:49 +0530

    changes post studunt json

--- src/main/java/com/demo/resorces/StudentResource.java
+++ src/main/java/com/demo/resorces/StudentResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.demo.model.Student;
@@ -13,20 +14,20 @@ public class StudentResource {
 	
 	
 	@GET
-	@Path("/get")
+	@Path("/xmlget")
 	@Produces("application/xml")
 	public Student getstudent(){
 		
 		Student student=new Student();
 		
 		student.setAddress("bangalore");
-		student.setName("svr0");
+		student.setName("svr");
 		student.setId(32);
 		
 		return student;
 	}
 	@POST
-	@Path("/post")
+	@Path("/xmlpost")
 	@Produces("application/xml")
 	public Response poststudent(Student student){
 		
@@ -35,6 +36,27 @@ public class StudentResource {
 		
 		return Response.status(200).entity(result).build();
 	}
+	@GET
+	@Path("/jsonget")
+	@Produces("application/json")
+	public Student getjsonstudent(){
+		
+		Student student=new Student();
+		
+		student.setAddress("bangalore");
+		student.setName("svr");
+		student.setId(32);
+		
+		return student;
+	}
+	@POST
+	@Path("/jsonpost")
+	@Produces("application/json")
+	public Response postjsonstudent(Student student){
+		
 	
-
+		String result="saved student"+student;
+		
+		return Response.status(200).entity(result).build();
+	}
 }
