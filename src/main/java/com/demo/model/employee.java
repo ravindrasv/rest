494c5b1f7529290174a9bb8abf7b6ad19f6ba136
ravi-PC\ravi <reddy_s35@yahoo.in>
2017-05-22 21:26:38 +0530

    added employee model

---
+++ src/main/java/com/demo/model/employee.java
@@ -0,0 +1,19 @@
+package com.demo.model;
+
+public class employee {
+	String name;
+	int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
